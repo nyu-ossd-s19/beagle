7bb0b1880569ac68e062dd9095d4a6e0549a45dc
alishasonawalla <asonawalla@nyu.edu>
2019-04-30 13:09:17 -0400

    Skeleton for user class

---
+++ beagle/nodes/user.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from typing import DefaultDict, List, Optional
+
+from beagle.nodes import Edge, Node
+
+class Assign_Rights(Edge):
+    __name__ = "Assign Rights To"
+
+    # Still contemplating whether this should be in the edge
+    rights_assigned: Optional[str]
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class User(Node):
+    __name__ = "User"
+    __color__ = "F79805"  # orange
+
+    user_name: Optional[str]
+
+    assigned_rights: DefaultDict["User", Assign_Rights]
+
+    def __init__(
+        self,
+        user_name: str = None,
+    ) -> None:
+        self.user_name = user_name
+        self.assigned_rights = defaultdict(Assign_Rights)
+
+    @property
+    def edges(self) -> List[DefaultDict]:
+        return [self.assigned_rights]
+
+    @property
+    def _display(self) -> str:
+        return self.user_name or super()._display
